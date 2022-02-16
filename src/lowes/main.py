341e072e78654f7f66e7e552f9e9976eabe7341e
ravindra varidireddy <reddyravi_mca2002@yahoo.com>
2022-02-15 20:56:12 -0500

    myfirst commit

---
+++ src/lowes/main.py
@@ -0,0 +1,22 @@
+# This is a sample Python script.
+
+# Press Shift+F10 to execute it or replace it with your code.
+# Press Double Shift to search everywhere for classes, files, tool windows, actions, and settings.
+
+
+def my_function(food):
+    for fruit in food:
+        print(fruit)
+
+if __name__ == '__main__':
+
+    fruits = ["apple", "banana", "cherry"]
+
+    my_function(fruits)
+
+
+
+
+
+
+
